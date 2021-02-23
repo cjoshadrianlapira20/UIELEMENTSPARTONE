2c1c847ee28ad7307b37e4a5dbb65b9b74d3cc62
Home PC <cjoshadrianlapira20@gmail.com>
2021-02-23 10:59:03 +0800

    FINISHED

---
+++ app/src/main/java/com/example/uielementspart1/ThirdActivity.kt
@@ -0,0 +1,19 @@
+package com.example.uielementspart1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+
+        findViewById<TextView>(R.id.finalFirstName).setText("First Name: ${(intent.getStringExtra("firstName"))}")
+        findViewById<TextView>(R.id.finalLastName).setText("Last Name: ${( intent.getStringExtra("lastName"))}")
+        findViewById<TextView>(R.id.finalEmail).setText("Email: ${( intent.getStringExtra("email"))}")
+        findViewById<TextView>(R.id.finalAge).setText("Age: ${( intent.getStringExtra("age"))}")
+        findViewById<TextView>(R.id.finalPhoneNumber).setText("Number: ${ (intent.getStringExtra("phoneNumber"))}")
+        findViewById<TextView>(R.id.finalBirthdate).setText("Birthday: ${( intent.getStringExtra("birthDate") )}")
+    }
+}
